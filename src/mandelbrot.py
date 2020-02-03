eda8f51be4cda488008661ada5dad1d76557db38
Javad Noorbakhsh <javad.noorbakhsh@gmail.com>
2020-02-03 00:21:12 -0500

    fix zoom

--- src/mandelbrot.py
+++ src/mandelbrot.py
@@ -17,12 +17,17 @@ def mandelbrot(creal, cimag, maxiter):
 
 
 @jit
-def mandelbrot_set(xmin, xmax, ymin, ymax, width, height, maxiter, scale=1.0):
-    scale = np.float(scale)
-    r1 = np.linspace(xmin / scale, xmax / scale, width)
-    r2 = np.linspace(ymin / scale, ymax / scale, height)
+def mandelbrot_set_(xmin, xmax, ymin, ymax, width, height, maxiter):
+    r1 = np.linspace(xmin, xmax, width)
+    r2 = np.linspace(ymin, ymax, height)
     n3 = np.empty((width, height))
     for i in range(width):
         for j in range(height):
             n3[i,j] = mandelbrot(r1[i],r2[j], maxiter)
     return (r1,r2,n3)
+
+def mandelbrot_set(x, y, unscaled_width=2.5, N=1000, maxiter=100, scale=0.0):
+    L = unscaled_width * (2**-scale) / 2
+    
+    m = mandelbrot_set_(x-L, x+L, y-L, y+L, N, N, maxiter)
+    return m
--- src/plotting.py
+++ src/plotting.py
@@ -3,17 +3,17 @@ from matplotlib import colors
 import numpy as np
 import src.mandelbrot as mb
 
-def mandelbrot_image(xmin, xmax, ymin, ymax, scale=1.0, width=3, height=3, maxiter=80, cmap='hot', dpi = 72):
+def mandelbrot_image(x, y, width=7, height=7, unscaled_width=2.5, N=1000, maxiter=100, scale=0.0, cmap='hot', dpi = 72):
     img_width = dpi * width
     img_height = dpi * height
-    x,y,z = mb.mandelbrot_set(xmin, xmax, ymin, ymax, img_width, img_height, maxiter, scale=scale)
-    
+    x,y,z = mb.mandelbrot_set(x, y, unscaled_width=unscaled_width, N=N, maxiter=maxiter, scale=scale)
+
     fig, ax = plt.subplots(figsize=(width, height), dpi=dpi)
-    ticks = np.arange(0,img_width,3*dpi)
-    x_ticks = xmin + (xmax-xmin)*ticks/img_width
-    plt.xticks(ticks, x_ticks)
-    y_ticks = ymin + (ymax-ymin)*ticks/img_width
-    plt.yticks(ticks, y_ticks)
-    
+#     ticks = np.arange(0,img_width,3*dpi)
+#     x_ticks = xmin + (xmax-xmin)*ticks/img_width
+#     plt.xticks(ticks, x_ticks/scale)
+#     y_ticks = ymin + (ymax-ymin)*ticks/img_width
+#     plt.yticks(ticks, y_ticks/scale)
+    plt.axis('off')
     norm = colors.PowerNorm(0.3)
     ax.imshow(z.T, cmap=cmap, origin='lower', norm=norm)
